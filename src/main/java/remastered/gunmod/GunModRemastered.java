e2c3544a9a8f2145a6091cd48b76267935758741
Peter C <peter15@qrts.com>
2022-05-12 14:01:54 -0400

    Refactored project to GunMod; changed build.gradle

--- src/main/java/remastered/gunmod/GunModRemastered.java
+++ src/main/java/remastered/gunmod/GunModRemastered.java
@@ -1,14 +1,14 @@
-package com.example.examplemod;
+package remastered.gunmod;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = GunModRemastered.MODID, version = GunModRemastered.VERSION)
+public class GunModRemastered
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "gunmod_remastered";
     public static final String VERSION = "1.0";
     
     @EventHandler
